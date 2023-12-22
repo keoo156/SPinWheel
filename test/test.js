0a7fd6740dae5892ed96698d4d699b2c43fdc7fb
張大胖 <someonewasthere75@gmail.com>
2023-12-22 09:44:49 +0800

    大改

--- test/test.js
+++ test/test.js
@@ -2,53 +2,81 @@ $(document).ready(function () {
   let element = $(".cube");
   let strBtn = $(".start");
   let newClass = "newCube";
-  let loopTime = 0;
-  let round;
-  let end;
+  let end; //設定要走幾步
 
-  function addClass(LT, targetIndex) {
-    let promiseChain = Promise.resolve();
-    loopTime++;
+  // function spinWheel(LT, targetIndex) {
+  //   let promiseChain = Promise.resolve();
+  //   loopTime++;
 
-    for (let i = 0; i < element.length; i++) {
-      promiseChain = promiseChain.then(() => {
-        return new Promise((resolve) => {
-          //判斷是否是終點
-          if (loopTime === LT && i === targetIndex) {
-            alert("end");
-            loopTime = 0;
-            $(strBtn).prop("disabled", false);
-            return;
-          }
+  //   for (let i = 0; i < element.length; i++) {
+  //     promiseChain = promiseChain.then(() => {
+  //       return new Promise((resolve) => {
+  //         //判斷是否是終點
+  //         if (loopTime === LT && i === targetIndex) {
+  //           alert("end");
+  //           loopTime = 0;
+  //           //啟動按鈕
+  //           $(strBtn).prop("disabled", false);
+  //           return;
+  //         }
 
-          setTimeout(function () {
-            console.log(i);
+  //         if (loopTime === LT && i >= targetIndex - 3) {
+  //           setClass(i, 2000, resolve);
+  //         } else {
+  //           setClass(i, 1000, resolve);
+  //         }
+  //       });
+  //     });
+  //   }
+  //   //判斷圈數
+  //   if (loopTime <= LT) {
+  //     promiseChain = promiseChain.then(() => spinWheel(round, end));
+  //   }
+  // }
 
-            //清除不要的class
-            $(element).removeClass(newClass);
+  async function spinWheel(targetIndex) {
+    let spinTime = 0;
+    let index = 0;
 
-            $(element[i]).addClass(newClass);
+    while (spinTime < targetIndex) {
+      await new Promise((resolve) => {
+        if (spinTime >= targetIndex - 3) {
+          setClass(index, 1000, resolve);
+        } else {
+          setClass(index, 500, resolve);
+        }
+        index++;
+        spinTime++;
 
-            resolve();
-          }, random($(element[i]).text()));
-        });
+        if (index === element.length) {
+          index = 0;
+        }
       });
     }
-    //判斷圈數
-    if (loopTime <= LT) {
-      promiseChain = promiseChain.then(() => addClass(round, end));
-    }
   }
-  //取得隨機時間
-  function random(i) {
-    return 500 * i;
+
+  function setClass(i, time, resolve) {
+    setTimeout(function () {
+      //清除不要的class
+      $(element).removeClass(newClass);
+      //
+      $(element[i]).addClass(newClass);
+      resolve();
+    }, time);
   }
 
   $(strBtn).click(function () {
+    //讓按鈕失效
     $(strBtn).prop("disabled", true);
-    round = Math.floor(Math.random() * 2) + 1;
-    end = Math.floor(Math.random() * element.length - 1) + 1;
-    addClass(round, end);
-    console.log(round, end);
+
+    end = Math.floor(Math.random() * (15 - 8 + 1)) + 5;
+    spinWheel(9).then(() => {
+      setTimeout(() => {
+        alert("結束");
+        $(strBtn).prop("disabled", false);
+      }, 1000);
+    });
+
+    console.log(end);
   });
 });
